54ae5558675de828f629090fbdaad2132f423f03
Carson West <c7933552528@gmail.com>
2024-01-22 11:51:17 -0500

    Making local import of Team data from json

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ In the future:
 - Make methods to add people and times
 - Make Team read and writable(done) from a file
 """
+import json
 import requests
 def string_to_time(str):
     
@@ -16,31 +17,47 @@ def string_to_time(str):
             f+= int(str.split(".")[1])/100
       return f/86400
 
+
+
+
 class Team: # Team Class, contains the name of the team, and a list of Swimmer objects who are on the team. Usage: team = Team("<url of home page>")
-    def __init__(self, url):
-        
-        self.url = url # for safekeeping the future
-        
-        self.html_m = requests.get(url+"roster/?page=1&gender=M&season_id=27&sort=perf").text.split("<tbody>")[1].split("</tbody>")[0].split("<tr \n          >\n") # Cuts down the HTML page to just the roster
-        self.html_f = requests.get(url+"roster/?page=1&gender=F&season_id=27&sort=perf").text.split("<tbody>")[1].split("</tbody>")[0].split("<tr \n          >\n")
-        
-        self.html_m.pop(0)
-        self.html_f.pop(0)
-        
-        self.name = requests.get(url+"roster/?page=1&gender=M&season_id=27&sort=perf").text.split('<meta property="og:title" content="')[1].split('"')[0]
-        
-        self.team_m = []
-        self.team_f = []
-        for x in self.html_m:
-            if "&ndash;" in x.split("</td>")[4]: # if they havent scored points, ignore
-                break
-            self.team_m.append(Swimmer("https://www.swimcloud.com"+x.split("</td>")[1].split("href=\"")[1].split("\">")[0]))
-            print(f"Added {self.team_m[-1].name}")
-        for x in self.html_f:
-            if "&ndash;" in x.split("</td>")[4]: # if they havent scored points, ignore
-                break
-            self.team_f.append(Swimmer("https://www.swimcloud.com"+x.split("</td>")[1].split("href=\"")[1].split("\">")[0]))
-            print(f"Added {self.team_f[-1].name}")
+    def __init__(self, url,u = "l"):
+        if u == 'j':
+            print('importing list to object') # doesnt work yet
+
+            t = json.loads(open(url,"r").read())
+            self.name = t[0][0]
+            self.url = t[0][1]
+
+            self.team_m = []
+            self.team_f = []
+            for x in t[1]:
+                self.team_m.append(Swimmer(x))
+
+        else:
+            print('Url detected, finding data')
+            self.url = url # for safekeeping the future
+            
+            html_m = requests.get(url+"roster/?page=1&gender=M&season_id=27&sort=perf").text.split("<tbody>")[1].split("</tbody>")[0].split("<tr \n          >\n") # Cuts down the HTML page to just the roster
+            html_f = requests.get(url+"roster/?page=1&gender=F&season_id=27&sort=perf").text.split("<tbody>")[1].split("</tbody>")[0].split("<tr \n          >\n")
+            
+            html_m.pop(0)
+            html_f.pop(0)
+            
+            self.name = requests.get(url+"roster/?page=1&gender=M&season_id=27&sort=perf").text.split('<meta property="og:title" content="')[1].split('"')[0]
+            
+            self.team_m = []
+            self.team_f = []
+            for x in html_m:
+                if "&ndash;" in x.split("</td>")[4]: # if they havent scored points, ignore
+                    break
+                self.team_m.append(Swimmer("https://www.swimcloud.com"+x.split("</td>")[1].split("href=\"")[1].split("\">")[0]))
+                print(f"Added {self.team_m[-1].name}")
+            for x in html_f:
+                if "&ndash;" in x.split("</td>")[4]: # if they havent scored points, ignore
+                    break
+                self.team_f.append(Swimmer("https://www.swimcloud.com"+x.split("</td>")[1].split("href=\"")[1].split("\">")[0]))
+                print(f"Added {self.team_f[-1].name}")
 
     def __str__(self):
         f = ''
@@ -63,25 +80,27 @@ class Team: # Team Class, contains the name of the team, and a list of Swimmer o
             f += x.save()
         f = f[0:len(f)-2] # Gets rid of the last comma
         f += '\n\t]\n]'
-        file = open(f"{self.name}.txt",'w')
+        file = open(f"{self.name.replace(' ','_')}.json",'w')
         
         file.write(f)
 
     
-    
-    
 class Swimmer:
-    def __init__(self, url):
-        self.url = url
-        html = requests.get(url).text.split("<body")[1].split('<span class="u-mr-">')[1]
-        self.name = html.split('</span>')[0]
-        self.times = {}
-        table = html.split("<tbody>")[2].split("</tbody>")[0].split("</tr>")
-        table.pop(-1)
-        for x in table:
-            
-            td = x.split("</td>")
-            self.times[td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0]] = [string_to_time(td[1].split("</a>")[0].split('">')[2]),td[1].split("</a>")[0].split('">')[2]]
+    def __init__(self, url, u = "u"):
+        if u == "l":
+            self.name = url[]
+
+        else:
+            self.url = url
+            html = requests.get(url).text.split("<body")[1].split('<span class="u-mr-">')[1]
+            self.name = html.split('</span>')[0]
+            self.times = {}
+            table = html.split("<tbody>")[2].split("</tbody>")[0].split("</tr>")
+            table.pop(-1)
+            for x in table:
+                
+                td = x.split("</td>")
+                self.times[td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0]] = [string_to_time(td[1].split("</a>")[0].split('">')[2]),td[1].split("</a>")[0].split('">')[2]]
     def __str__(self):
         f = ''
         f += f'{self.name}\n{self.url}\n'
@@ -92,9 +111,13 @@ class Swimmer:
     def save(self):
         f = f'\t\t[\n\t\t\t"{self.name}", "{self.url}",\n\t\t\t'+'{'
         for x in self.times:             
-            f += f'\n\t\t\t\t"{str(x)}" : {self.times[x]},'
+            f += f'\n\t\t\t\t"{str(x)}" : {self.times[x]},'.replace("'",'"')
+        f = f[0:len(f)-1]
         f += '\n\t\t\t}\n\t\t],\n'
         return f
     
+
+
+team = Team("./lc.json",'j')
 
 
