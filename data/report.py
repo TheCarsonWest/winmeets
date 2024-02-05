aaee23f62898d3e552757bde741526babb841f33
Carson West <c7933552528@gmail.com>
2024-02-05 08:32:31 -0500

    Pre state stalking of every other team at states

--- data/report.py
+++ data/report.py
@@ -17,156 +17,183 @@ def string_to_time(str):
       return f/86400
 
 
-names_list_m = []
-names_list_f = []
 
 importer = [["Name","Event","Time","Qualification","Meet","Date","Fixed Time","Sex","RQAL"]]
 
+urls = ['https://www.swimcloud.com/team/10026271/',
+'https://www.swimcloud.com/team/4444/',
+'https://www.swimcloud.com/team/10001012/',
+'https://www.swimcloud.com/team/4391/',
+'https://www.swimcloud.com/team/10001011/',
+'https://www.swimcloud.com/team/4342/',
+'https://www.swimcloud.com/team/4334/',
+'https://www.swimcloud.com/team/4512/',
+'https://www.swimcloud.com/team/4329/',
+'https://www.swimcloud.com/team/4341/',
+'https://www.swimcloud.com/team/4338/',
+'https://www.swimcloud.com/team/10009151/',
+'https://www.swimcloud.com/team/10001017/',
+'https://www.swimcloud.com/team/4468/',
+'https://www.swimcloud.com/team/7492/',
+'https://www.swimcloud.com/team/4473/',
+'https://www.swimcloud.com/team/4517/',
+'https://www.swimcloud.com/team/4470/',
+'https://www.swimcloud.com/team/4518/',
+'https://www.swimcloud.com/team/4472/',
+'https://www.swimcloud.com/team/4469/',
+'https://www.swimcloud.com/team/10024716/',
+'https://www.swimcloud.com/team/10009150/',
+'https://www.swimcloud.com/team/4463/',
+'https://www.swimcloud.com/team/4387/',
+'https://www.swimcloud.com/team/4324/',
+'https://www.swimcloud.com/team/4327/',
+'https://www.swimcloud.com/team/7495/']
 
-url = input("Input team home page\n > ")
-
-
-url_m = url+"roster/?page=1&gender=M&season_id=27&sort=perf"
-url_f = url+"roster/?page=1&gender=F&season_id=27&sort=perf"
-
-
-
-html = requests.get(url_m).text
-csv_filename = html.split('<meta property="og:title" content="')[1].split('"')[0].replace(" ","_")
-print(csv_filename)
-
-html_m = html.split("<tbody>")[1].split("</tbody>")[0].split("<tr \n          >\n") # gets html page of roster
-html_m.pop(0) # the first raw has nothing in it, popped it
-
-
-each_m = []
-
-
-for x in html_m: # creates a list of swimcloud links for everyone with a point value
-        if "&ndash;" in x.split("</td>")[4]: # if they havent scored points, ignore
-            break
-        each_m.append("https://www.swimcloud.com"+x.split("</td>")[1].split("href=\"")[1].split("\">")[0])
-print(f"Found {len(each_m)} male swimmers")
-
-# repeats the exact same steps but for the women's team
-
-html_f = requests.get(url_f).text.split("<tbody>")[1].split("</tbody>")[0].split("<tr \n          >\n") # gets html page of roster
-html_f.pop(0) # the first raw has nothing in it, popped it
-
-
-each_f = []
-f = 0
-for x in html_f: # creates a list of swimcloud links for everyone with a point value
-        if "&ndash;" in x.split("</td>")[4]: # if they havent scored points, ignore
-            break
-        each_f.append("https://www.swimcloud.com"+x.split("</td>")[1].split("href=\"")[1].split("\">")[0])
-        f +=1
-print(f"Found {f} female swimmers")        
-
-l = 2
-
-for x in each_m:
-      
-      s_html = requests.get(x).text.split("<body")[1].split('<span class="u-mr-">')[1] # Gets the HTML and cuts it down right to where we need stuff
-      s_name = s_html.split('</span>')[0] # Their name, for importing
-
-      #print(f"Found {s_name}'s Times")
-      s_table = s_html.split("<tbody>")[2].split("</tbody>")[0].split("</tr>")
-      s_table.pop(-1)
-      print(f"Found {str(len(s_table))} events for {s_name}")
-      names_list_m.append(s_name)
-
-
-      for i in s_table: 
-            r = [s_name]
-            s_td = i.split("</td>") # we are going to ignore this variable name
-            if s_td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0] in hs_events:
-                if len(s_td)==6:
-
-                        r.append(s_td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0])
-                        r.append(s_td[1].split("</a>")[0].split('">')[2])
-                        r.append("")
-                        r.append(s_td[3].split("</a>")[0].split('">')[2])
-                        r.append(s_td[4].split('<td class="u-text-truncate">')[1])
-                else:        
-                        r.append(s_td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0])
-                        r.append(s_td[1].split("</a>")[0].split('">')[2])
-                        r.append("")
-                        r.append(s_td[2].split("</a>")[0].split('">')[2])
-                        r.append(s_td[3].split('<td class="u-text-truncate">')[1])
-
-                r.append(string_to_time(r[2]))
-                r.append(True)
-                r.append(f"=G{l}<HLOOKUP(B{l},Table7[#All],IF('Time Sheet'!H{l},3,2),FALSE)")
-
-                importer.append(r)
-                l+=1
-# low tech solution
-for x in each_f:
-      
-      s_html = requests.get(x).text.split("<body")[1].split('<span class="u-mr-">')[1] # Gets the HTML and cuts it down right to where we need stuff
-      s_name = s_html.split('</span>')[0] # Their name, for importing
-
-      #print(f"Found {s_name}'s Times")
-      s_table = s_html.split("<tbody>")[2].split("</tbody>")[0].split("</tr>")
-      s_table.pop(-1)
-      print(f"Found {str(len(s_table))} events for {s_name}")
-      names_list_f.append(s_name)
-
-
-      for i in s_table: 
-            r = [s_name]
-            s_td = i.split("</td>") # we are going to ignore this variable name
-            if s_td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0] in hs_events:
-                if len(s_td)==6:
-
-                        r.append(s_td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0])
-                        r.append(s_td[1].split("</a>")[0].split('">')[2])
-                        r.append("")
-                        r.append(s_td[3].split("</a>")[0].split('">')[2])
-                        r.append(s_td[4].split('<td class="u-text-truncate">')[1])
-                else:        
-                        r.append(s_td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0])
-                        r.append(s_td[1].split("</a>")[0].split('">')[2])
-                        r.append("")
-                        r.append(s_td[2].split("</a>")[0].split('">')[2])
-                        r.append(s_td[3].split('<td class="u-text-truncate">')[1])
-
-                r.append(string_to_time(r[2]))
-                r.append(False)
-                r.append(f"=G{l}<HLOOKUP(B{l},Table7[#All],IF('Time Sheet'!H{l},3,2),FALSE)")
-
-                importer.append(r)
-                l+=1
-
-
-
-# Read the CSV file
-
-template_file = 'template.xlsx'  # Replace with your template Excel file name
-
-# Load the template Excel file
-template_wb = load_workbook(template_file)
-
-# Get the desired sheet from the template file to combine data
-time_sheet = template_wb['Time Sheet']  # Assuming the sheet is named "Time Sheet"
-# Write data to the template sheet
-for row_data in importer:
-    time_sheet.append(row_data)
-print("Time Sheet Created)")
-
-sheet_m = template_wb['Boys']
-sheet_f = template_wb['Girls']
-i = 2
-for n in names_list_m:
-     sheet_m[f"A{i}"] = n 
-     i+=1
-i = 2
-for n in names_list_f:
-     sheet_f[f"A{i}"] = n
-     i+=1
-
-print('names put in the sheet')
-template_wb.save(csv_filename+'.xlsx')
-print(f"Workbook Saved as {csv_filename}.xlsx")
-        
+for url in urls:
+  
+  url_m = url+"roster/?page=1&gender=M&season_id=27&sort=perf"
+  url_f = url+"roster/?page=1&gender=F&season_id=27&sort=perf"
+  
+  
+  names_list_m = []
+  names_list_f = []
+  
+  html = requests.get(url_m).text
+  csv_filename = html.split('<meta property="og:title" content="')[1].split('"')[0].replace(" ","_")
+  print(csv_filename)
+  
+  html_m = html.split("<tbody>")[1].split("</tbody>")[0].split("<tr \n          >\n") # gets html page of roster
+  html_m.pop(0) # the first raw has nothing in it, popped it
+  
+  
+  each_m = []
+  
+  
+  for x in html_m: # creates a list of swimcloud links for everyone with a point value
+          if "&ndash;" in x.split("</td>")[4]: # if they havent scored points, ignore
+              break
+          each_m.append("https://www.swimcloud.com"+x.split("</td>")[1].split("href=\"")[1].split("\">")[0])
+  print(f"Found {len(each_m)} male swimmers")
+  
+  # repeats the exact same steps but for the women's team
+  
+  html_f = requests.get(url_f).text.split("<tbody>")[1].split("</tbody>")[0].split("<tr \n          >\n") # gets html page of roster
+  html_f.pop(0) # the first raw has nothing in it, popped it
+  
+  
+  each_f = []
+  f = 0
+  for x in html_f: # creates a list of swimcloud links for everyone with a point value
+          if "&ndash;" in x.split("</td>")[4]: # if they havent scored points, ignore
+              break
+          each_f.append("https://www.swimcloud.com"+x.split("</td>")[1].split("href=\"")[1].split("\">")[0])
+          f +=1
+  print(f"Found {f} female swimmers")        
+  
+  l = 2
+  
+  for x in each_m:
+  
+        s_html = requests.get(x).text.split("<body")[1].split('<span class="u-mr-">')[1] # Gets the HTML and cuts it down right to where we need stuff
+        s_name = s_html.split('</span>')[0] # Their name, for importing
+  
+        #print(f"Found {s_name}'s Times")
+        s_table = s_html.split("<tbody>")[2].split("</tbody>")[0].split("</tr>")
+        s_table.pop(-1)
+        print(f"Found {str(len(s_table))} events for {s_name}")
+        names_list_m.append(s_name)
+  
+  
+        for i in s_table: 
+              r = [s_name]
+              s_td = i.split("</td>") # we are going to ignore this variable name
+              if s_td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0] in hs_events:
+                  if len(s_td)==6:
+  
+                          r.append(s_td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0])
+                          r.append(s_td[1].split("</a>")[0].split('">')[2])
+                          r.append("")
+                          r.append(s_td[3].split("</a>")[0].split('">')[2])
+                          r.append(s_td[4].split('<td class="u-text-truncate">')[1])
+                  else:        
+                          r.append(s_td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0])
+                          r.append(s_td[1].split("</a>")[0].split('">')[2])
+                          r.append("")
+                          r.append(s_td[2].split("</a>")[0].split('">')[2])
+                          r.append(s_td[3].split('<td class="u-text-truncate">')[1])
+  
+                  r.append(string_to_time(r[2]))
+                  r.append(True)
+                  r.append(f"=G{l}<HLOOKUP(B{l},Table7[#All],IF('Time Sheet'!H{l},3,2),FALSE)")
+  
+                  importer.append(r)
+                  l+=1
+  # low tech solution
+  for x in each_f:
+  
+        s_html = requests.get(x).text.split("<body")[1].split('<span class="u-mr-">')[1] # Gets the HTML and cuts it down right to where we need stuff
+        s_name = s_html.split('</span>')[0] # Their name, for importing
+  
+        #print(f"Found {s_name}'s Times")
+        s_table = s_html.split("<tbody>")[2].split("</tbody>")[0].split("</tr>")
+        s_table.pop(-1)
+        print(f"Found {str(len(s_table))} events for {s_name}")
+        names_list_f.append(s_name)
+  
+  
+        for i in s_table: 
+              r = [s_name]
+              s_td = i.split("</td>") # we are going to ignore this variable name
+              if s_td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0] in hs_events:
+                  if len(s_td)==6:
+  
+                          r.append(s_td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0])
+                          r.append(s_td[1].split("</a>")[0].split('">')[2])
+                          r.append("")
+                          r.append(s_td[3].split("</a>")[0].split('">')[2])
+                          r.append(s_td[4].split('<td class="u-text-truncate">')[1])
+                  else:        
+                          r.append(s_td[0].split('<td class="u-text-truncate">')[1].split('class="js-event-link">')[1].split("</")[0])
+                          r.append(s_td[1].split("</a>")[0].split('">')[2])
+                          r.append("")
+                          r.append(s_td[2].split("</a>")[0].split('">')[2])
+                          r.append(s_td[3].split('<td class="u-text-truncate">')[1])
+  
+                  r.append(string_to_time(r[2]))
+                  r.append(False)
+                  r.append(f"=G{l}<HLOOKUP(B{l},Table7[#All],IF('Time Sheet'!H{l},3,2),FALSE)")
+  
+                  importer.append(r)
+                  l+=1
+  
+  
+  
+  # Read the CSV file
+  
+  template_file = './template.xlsx'  # Replace with your template Excel file name
+  
+  # Load the template Excel file
+  template_wb = load_workbook(template_file)
+  
+  # Get the desired sheet from the template file to combine data
+  time_sheet = template_wb['Time Sheet']  # Assuming the sheet is named "Time Sheet"
+  # Write data to the template sheet
+  for row_data in importer:
+      time_sheet.append(row_data)
+  print("Time Sheet Created)")
+  
+  sheet_m = template_wb['Boys']
+  sheet_f = template_wb['Girls']
+  i = 2
+  for n in names_list_m:
+       sheet_m[f"A{i}"] = n 
+       i+=1
+  i = 2
+  for n in names_list_f:
+       sheet_f[f"A{i}"] = n
+       i+=1
+  
+  print('names put in the sheet')
+  template_wb.save(csv_filename+'.xlsx')
+  print(f"Workbook Saved as {csv_filename}.xlsx")
+  
--- playground.py
+++ playground.py
@@ -1,6 +1,2 @@
 from data import *
 
-while True:
-    i = float("0."+input("> "))
-    print(time_to_string(i))
-
